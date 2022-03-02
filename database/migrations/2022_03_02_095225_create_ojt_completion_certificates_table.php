f5e37ecf7b97d0c1b2c03a928096f8f3e7aaa154
Ankit <bishtsonu251011@gmail.com>
2022-03-02 18:25:03 +0530

    Update 2022_03_02_095225_create_ojt_completion_certificates_table.php
    first commit in migration

--- database/migrations/2022_03_02_095225_create_ojt_completion_certificates_table.php
+++ database/migrations/2022_03_02_095225_create_ojt_completion_certificates_table.php
@@ -14,8 +14,13 @@ class CreateOjtCompletionCertificatesTable extends Migration
     public function up()
     {
         Schema::create('ojt_completion_certificates', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+            $table->string('ojt_id',50);
+            $table->string('candidate_id',40)->nullable();
+            $table->string('ojt_certificate',50);
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamp('created_on')->nullable();
+            $table->timestamp('updated_on')->nullable();
         });
     }
 
