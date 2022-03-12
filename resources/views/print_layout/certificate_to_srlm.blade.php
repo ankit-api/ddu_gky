9e38dc3ab82e07a9ef3c5bd58e08ba7e5e4c48ad
Ankit <bishtsonu251011@gmail.com>
2022-03-12 14:05:27 +0530

    Made separate folder for print layout

---
+++ resources/views/print_layout/certificate_to_srlm.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h3>SF 6.1A: Letter to SRLM seeking information on mobilization plan</h3>
+                <span>(To be printed on PIA Letterhead)</span>
+            </div>
+            <div class="col-12">
+                <p>
+                    To,<br>
+                    Name of the Officer <br>
+                    Designation <br>
+                    Organization <br>
+                    Address
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
